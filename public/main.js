3c29def38106bc6f1d07f6f97683f727fa97a2bc
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-05-16 16:58:48 -0400

    added mission info

--- public/main.js
+++ public/main.js
@@ -10,14 +10,19 @@ const main = () => {
       document.querySelector('.copyright').textContent = 'copyright: ' + message.copyright
       document.querySelector('.title').textContent = 'title: ' + message.title
     })
-  // fetch('https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming')
-  //   .then(resp => {
-  //     console.log(resp)
-  //     return resp.json()
-  //   })
-  //   .then(message => {
-  //       console.log(message)
-  //     }
-}
+  fetch('https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming')
+    .then(resp => {
+      console.log(resp)
+      return resp.json()
+    })
+    .then(message => {
+      console.log(message)
 
+      document.querySelector('.name').textContent = message[0].mission_name
+      document.querySelector('.description').textContent = message[0].details
+      document.querySelector('.time').textContent = message[0].launch_date_utc
+      document.querySelector('.location').textContent = message[0].launch_site.site_name_long
+
+    })
+}
 document.addEventListener('DOMContentLoaded', main)

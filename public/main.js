2fadc808ad542e1919ad656e58a9b7b8f7d4b477
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-05-16 15:13:41 -0400

    added api pic

--- public/main.js
+++ public/main.js
@@ -4,6 +4,16 @@ const main = () => {
       console.log(resp)
       return resp.json()
     })
+    .then(message => {
+      console.log(message)
+      document.querySelector('.api-pic').style.backgroundImage = 'url(' + message.url + ')'
+      document.querySelector('.copyright').textContent = 'copyright: ' + message.copyright
+      document.querySelector('.title').textContent = 'title: ' + message.title
+    })
+  // fetch('https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming')
+  // .then(resp =>{
+
+  // })
 }
 
 document.addEventListener('DOMContentLoaded', main)

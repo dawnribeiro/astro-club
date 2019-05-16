108f65a9071264150cd94ca099635aa311e0606d
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-05-15 21:33:09 -0400

    started adding api img

--- public/main.js
+++ public/main.js
@@ -1,7 +1,13 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
+    .then(resp => {
+      console.log(resp)
+      return resp.json()
+    })
+    .then(image => {
+      console.log({
+        image
+      })
+    })
 }
-
 document.addEventListener('DOMContentLoaded', main)
